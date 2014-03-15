c6959e5bc6f713c5d947025900b0741bc3addfc7
Hugh Rawlinson <hugh@codeoclock.net>
2014-03-15 13:05:12 +0000

    initial commit

---
+++ src/gilbert.cpp
@@ -0,0 +1,194 @@
+#include "gilbert.h"
+#include "math.h"
+//--------------------------------------------------------------
+void gilbert::setup(){
+    
+    ofSetVerticalSync(true);
+	ofEnableSmoothing();
+    ofSetFrameRate(60);
+    ofBackground(59,89,152);
+    
+	initialBufferSize = 512;
+	sampleRate = 44100;
+	drawCounter = 0;
+	bufferCounter = 0;
+    
+    snare.loadSound("sounds/snare.wav");
+    kick.loadSound("sounds/kick.wav");
+    
+    setGUI1();
+    gui1->loadSettings("gui1Settings.xml");
+	
+	buffer = new float[initialBufferSize];
+	memset(buffer, 0, initialBufferSize * sizeof(float));
+    
+	// 0 output channels,
+	// 1 input channels
+	// 44100 samples per second
+	// 512 samples per buffer
+	// 1 buffer
+	ofSoundStreamSetup(0, 1, this, sampleRate, initialBufferSize, 1);
+    
+    for (int i = 0; i < NUM_WINDOWS; i++)
+	{
+		for (int j = 0; j < BUFFER_SIZE/2; j++)
+		{
+			freq[i][j] = 0;
+		}
+	}
+    
+    buffer = new float[initialBufferSize];
+	memset(buffer, 0, initialBufferSize * sizeof(float));
+}
+
+//--------------------------------------------------------------
+void gilbert::update(){
+
+}
+
+//--------------------------------------------------------------
+void gilbert::draw(){
+    float avg_power = 0.0f;
+    myfft.powerSpectrum(0, (int)BUFFER_SIZE/2, buffer, BUFFER_SIZE, &magnitude[0], &phase[0], &power[0], &avg_power);
+    
+    drawCounter++;
+    
+    ofPushStyle();
+    ofSetColor(255);
+    ofSetLineWidth(1);
+    
+    float y1 = ofGetHeight() * 0.5;
+    ofLine(0, y1, ofGetWidth(), y1);
+    
+	for(int i=0; i<initialBufferSize; i++){
+        float p = i / (float)(initialBufferSize-1);
+        float x = p * ofGetWidth();
+        float y2 = y1 + buffer[i] * 200;
+        
+		ofLine(x, y1, x, y2);
+	}
+    ofPopStyle();
+    
+    if(calcRMS()>0.1){
+        ofLog(OF_LOG_NOTICE,"SC: " + ofToString(calcSC()));
+        if(calcSC()>3000){
+            snare.play();
+        }
+        else{
+            kick.play();
+        }
+    }
+    
+    ofPushStyle();
+	ofSetColor(255);
+    ofDrawBitmapString("AP: " + ofToString(avg_power),20, ofGetHeight()-100);
+    ofDrawBitmapString("SC: " + ofToString(calcSC()),20, ofGetHeight()-80);
+    ofDrawBitmapString("RMS: " + ofToString(calcRMS()), 20, ofGetHeight()-60);
+    ofDrawBitmapString("Buffer Counter: " + ofToString(bufferCounter), 20, ofGetHeight()-40);
+    ofDrawBitmapString("Draw Counter: " + ofToString(drawCounter),20, ofGetHeight()-20);
+    ofPopStyle();
+}
+
+void gilbert::exit(){
+    gui1->saveSettings("gui1Settings.xml");
+    delete gui1;
+}
+
+//--------------------------------------------------------------
+void gilbert::keyPressed(int key){
+    ofLog(OF_LOG_NOTICE, "key: %d",key);
+}
+
+//--------------------------------------------------------------
+void gilbert::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void gilbert::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void gilbert::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void gilbert::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void gilbert::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void gilbert::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void gilbert::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void gilbert::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+//--------------------------------------------------------------
+void gilbert::audioIn(float *input, int bufferSize, int nChannels){
+    int minBufferSize = MIN(initialBufferSize, bufferSize);
+	for(int i=0; i<minBufferSize; i++) {
+		buffer[i] = input[i];
+	}
+	bufferCounter++;
+}
+
+//--------------------------------------------------------------
+float gilbert::calcRMS(){
+    float count = 0;
+    for(int i=0; i<initialBufferSize; i++){
+        count += pow(buffer[i],2);
+    }
+    count = count/initialBufferSize;
+    return sqrt(count);
+}
+
+//--------------------------------------------------------------
+float gilbert::calcSC(){
+    float centroid;
+    
+    float sumMags;
+    float sumFreqByMags;
+    
+    for(int i = 0; i< BUFFER_SIZE; i++){
+        sumMags += magnitude[i];
+        sumFreqByMags += magnitude[i]*(i*44100/BUFFER_SIZE);
+    }
+    
+    centroid = sumFreqByMags/sumMags;
+    
+    return centroid;
+}
+
+//--------------------------------------------------------------
+void gilbert::setGUI1(){
+    gui1 = new ofxUICanvas("");
+    gui1->setDrawBack(false);
+    
+    gui1->addSpectrum("SPECTRUM", power, 256, 0, 6, 298, 100);
+    gui1->addLabel("SAMPLES", OFX_UI_FONT_SMALL);
+    gui1->setGlobalButtonDimension(50);
+    gui1->addToggleMatrix("1X4 MATRIX", 1, 4, 73, 50);
+    gui1->autoSizeToFitWidgets();
+    ofAddListener(gui1->newGUIEvent,this,&gilbert::guiEvent);
+}
+
+//--------------------------------------------------------------
+void gilbert::guiEvent(ofxUIEventArgs &e){
+    ofLog(OF_LOG_NOTICE, "Thanks!");
+}
---
+++ src/gilbert.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "ofMain.h"
+#include "fft.h"
+#include "ofxUI.h"
+
+#define BUFFER_SIZE 512
+#define NUM_WINDOWS 80
+
+class gilbert : public ofBaseApp{
+
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    //UI
+    void setGUI1();
+    ofxUICanvas *gui1;
+    void guiEvent(ofxUIEventArgs &e);
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    void audioIn(float *input, int bufferSize, int nChannels);
+    
+    int initialBufferSize;
+    int sampleRate;
+    int drawCounter;
+    int bufferCounter;
+    float * buffer;
+    
+    float calcRMS();
+    float calcSC();
+    
+    // FFT vars
+    fft myfft;
+    
+    ofSoundPlayer snare;
+    ofSoundPlayer kick;
+    
+    float magnitude[BUFFER_SIZE];
+    float phase[BUFFER_SIZE];
+    float power[BUFFER_SIZE];
+    
+    float freq[NUM_WINDOWS][BUFFER_SIZE/2];
+    float freq_phase[NUM_WINDOWS][BUFFER_SIZE/2];
+		
+};
